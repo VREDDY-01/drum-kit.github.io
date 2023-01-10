e4f4fce623a4e0ecdf5b0accfc7967a0fc2e9518
K Vishnu Teja <54312804+VREDDY-01@users.noreply.github.com>
2023-01-11 03:33:11 +0530

    Added all files to the hub

---
+++ index.js
@@ -0,0 +1,54 @@
+var numberOfButtons = document.querySelectorAll("button").length;
+for(var i = 0;i<numberOfButtons;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function clicked() {
+        playMusic(this.innerHTML);
+        buttonAnimate(this.innerHTML);
+    })
+}
+
+document.addEventListener("keypress",function (ev) {
+    playMusic(ev.key);
+    buttonAnimate(ev.key);
+})
+
+function playMusic(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kickbass = new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function buttonAnimate(key) {
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("."+key).classList.remove("pressed");
+  },100)  
+}
